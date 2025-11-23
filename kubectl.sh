052407c4bd351657225eca03227084746badb337
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-11-23 14:29:57 +0530

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Docs : https://kubernetes.io/docs/tasks/tools/install-kubectl-linux
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
