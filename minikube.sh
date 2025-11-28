27f618673b5aa582c5ab99cae6f9018be3124ace
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-11-28 16:10:01 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -12,13 +12,14 @@ systemctl status docker
 # Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/kubectl
-sudo chmod +x /usr/local/bin/kubect
+sudo chmod +x /usr/local/bin/kubectl
 kubectl version --client
 
 # minikube
 # Docs : https://minikube.sigs.k8s.io/docs/start/?arch=%2Flinux%2Fx86-64%2Fstable%2Fbinary+download
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
 minikube start --driver=docker --force
 minikube version  
-minikube status  
+minikube status
