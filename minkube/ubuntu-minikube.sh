67e692c93b3f9b4f11a112fdc14ea551a5dc32b0
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-11-29 13:02:22 +0530

    Create ubuntu-minikube.sh

---
+++ minkube/ubuntu-minikube.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Update system and install required base packages
+sudo apt-get update -y
+sudo apt-get install curl wget -y
+
+# Docker
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Minikube
+# Docs : https://minikube.sigs.k8s.io/docs/start/?arch=%2Flinux%2Fx86-64%2Fstable%2Fbinary+download
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+sudo minikube start --driver=docker --force
+sudo minikube version
+minikube status
