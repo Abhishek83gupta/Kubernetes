c184f2af4b8487175d1603132045c210980abd05
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-12-10 01:39:52 +0530

    Create master.sh

---
+++ kubeadm/master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Execute ONLY on the "Master" Node
+
+# Initialize Kubernetes control-plane
+kubeadm init 
+
+# Configure kubectl for root user
+mkdir -p $HOME/.kube                           
+cp /etc/kubernetes/admin.conf $HOME/.kube/config   
+chown $(id -u):$(id -g) $HOME/.kube/config          
+
+# Install Calico (CNI)
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml  
+
+# Show join command for workers
+kubeadm token create --print-join-command           
